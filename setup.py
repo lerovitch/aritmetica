7b34a903e8e5bfeec1ba0f8fac4e3b3a24ffd563
Sergi Sorribas <sergisj@tid.es>
2012-11-26 10:06:42 +0100

    ADDED setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import  setup, find_packages
+
+__VERSION__ = '0.1.b'
+
+setup(
+    name = "aritmetica",
+    version = __VERSION__,
+    description = "aritmetica modules",
+    author = 'Sergi',
+    author_email = 'lerovitch@yahoo.es',
+    url = 'http://www.tid.es',
+    py_modules = ["hotel", "suma_consecutius"]
+)
+
